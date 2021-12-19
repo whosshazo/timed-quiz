df5101bec707a898aee332cd1f00a53946a81d7a
whosshazo <ryandeshazo1@gmail.com>
2021-12-19 11:43:50 -0500

    questions in java

---
+++ JS/questions.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        title: "what is an object?",
+        choice: ["A. True or False", "B. If or Else", "C. Something Crazy", "D. Something Crazy"],
+        answer: "A. True or False"
+    },
+
+    {
+        title: "what is an object?",
+        choice: ["A. True or False", "B. If or Else", "C. Something Crazy", "D. Something Crazy"],
+        answer: "A. True or False"
+    }
+]
+
