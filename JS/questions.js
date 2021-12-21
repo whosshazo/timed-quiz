897263baf6d4738c3d0476e866dc03b18e85a1f3
whosshazo <ryandeshazo1@gmail.com>
2021-12-20 21:49:32 -0500

    questions

--- JS/questions.js
+++ JS/questions.js
@@ -1,14 +1,50 @@
 var questions = [
     {
-        title: "what is an object?",
-        choice: ["A. True or False", "B. If or Else", "C. Something Crazy", "D. Something Crazy"],
-        answer: "A. True or False"
+        title: "Inside which HTML element do we put the JavaScript?",
+        choice: ["A. <scripting>", "B. <javascript>", "C. <js>", "D. <script>"],
+        answer: "D. <script>"
     },
 
     {
-        title: "what is an object?",
-        choice: ["A. True or False", "B. If or Else", "C. Something Crazy", "D. Something Crazy"],
-        answer: "A. True or False"
+        title: "How can you add a comment in a JavaScript?",
+        choice: ["A. //This is a comment", "B. 'This is a comment'", "C. <!--This is a comment--!>", "D. <This is a comment>"],
+        answer: "A. //THis is a comment" 
+    },
+
+    {
+        title: "How does a FOR loop start?",
+        choice: ["A. for i = 1 to 5", "B. for(i = 0; i <= 5)", "C. for (i=0; i<=5; i++)", "D. for (i<=5;i++)"],
+        answer: "C. for (i=0; i<=5; i++"
+    },
+
+    {
+        title: "How do you call a function named myFunction?",
+        choice: ["A. myFunction()", "B. call function myFunction()", "C. call myFunction()", "D. Function()"],
+        answer: "A. myFunction()"
+    },
+
+    {
+        title: "The external JavaScript file must contain the <script> tag?",
+        choice: ["A. True", "B. False"],
+        answer: "B. False"
+    },
+
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choice: ["A. The <body> section", "B. The <head> section", "C. The <footer> section", "D. Both the <head> & <body> section"],
+        answer: "D. Both the <head> & <body> section "
+    },
+
+    {
+        title: "JavaScript is the same as Java?",
+        choice: ["A. Ture", "B. False"],
+        answer: "B. False"
+    },
+
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choice: ["A. onmouseclick", "B. onchange", "C. onclick", "D. onmouseover"],
+        answer: "C. onclick"
     }
 ]
 
