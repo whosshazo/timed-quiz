696f52c56e41793267cb7268c61d4002a4eb2c78
whosshazo <ryandeshazo1@gmail.com>
2022-02-27 10:42:47 -0500

    finalize loop section

--- JS/questions.js
+++ JS/questions.js
@@ -1,50 +1,69 @@
 var questions = [
-    {
-        title: "Inside which HTML element do we put the JavaScript?",
-        choice: [" <scripting>", " <javascript>", " <js>", " <script>"],
-        answer: " <script>"
-    },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: [" <scripting>", " <javascript>", " <js>", " <script>"],
+    answer: " <script>",
+  },
 
-    {
-        title: "How can you add a comment in a JavaScript?",
-        choice: [" //This is a comment", " 'This is a comment'", " <!--This is a comment--!>", " <This is a comment>"],
-        answer: " //THis is a comment" 
-    },
+  {
+    title: "How can you add a comment in a JavaScript?",
+    choices: [
+      " //This is a comment",
+      " 'This is a comment'",
+      " <!--This is a comment--!>",
+      " <This is a comment>",
+    ],
+    answer: " //This is a comment",
+  },
 
-    {
-        title: "How does a FOR loop start?",
-        choice: ["A. for i = 1 to 5", "B. for(i = 0; i <= 5)", "C. for (i=0; i<=5; i++)", "D. for (i<=5;i++)"],
-        answer: "C. for (i=0; i<=5; i++"
-    },
+  {
+    title: "How does a FOR loop start?",
+    choices: [
+      "A. for i = 1 to 5",
+      "B. for(i = 0; i <= 5)",
+      "C. for (i=0; i<=5; i++)",
+      "D. for (i<=5;i++)",
+    ],
+    answer: "C. for (i=0; i<=5; i++",
+  },
 
-    {
-        title: "How do you call a function named myFunction?",
-        choice: ["A. myFunction()", "B. call function myFunction()", "C. call myFunction()", "D. Function()"],
-        answer: "A. myFunction()"
-    },
+  {
+    title: "How do you call a function named myFunction?",
+    choices: [
+      "A. myFunction()",
+      "B. call function myFunction()",
+      "C. call myFunction()",
+      "D. Function()",
+    ],
+    answer: "A. myFunction()",
+  },
 
-    {
-        title: "The external JavaScript file must contain the <script> tag?",
-        choice: ["A. True", "B. False"],
-        answer: "B. False"
-    },
+  {
+    title: "The external JavaScript file must contain the <script> tag?",
+    choices: ["A. True", "B. False"],
+    answer: "B. False",
+  },
 
-    {
-        title: "Where is the correct place to insert a JavaScript?",
-        choice: ["A. The <body> section", "B. The <head> section", "C. The <footer> section", "D. Both the <head> & <body> section"],
-        answer: "D. Both the <head> & <body> section "
-    },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "A. The <body> section",
+      "B. The <head> section",
+      "C. The <footer> section",
+      "D. Both the <head> & <body> section",
+    ],
+    answer: "D. Both the <head> & <body> section ",
+  },
 
-    {
-        title: "JavaScript is the same as Java?",
-        choice: ["A. Ture", "B. False"],
-        answer: "B. False"
-    },
+  {
+    title: "JavaScript is the same as Java?",
+    choices: ["A. Ture", "B. False"],
+    answer: "B. False",
+  },
 
-    {
-        title: "Which event occurs when the user clicks on an HTML element?",
-        choice: ["A. onmouseclick", "B. onchange", "C. onclick", "D. onmouseover"],
-        answer: "C. onclick"
-    }
-]
-
+  {
+    title: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["A. onmouseclick", "B. onchange", "C. onclick", "D. onmouseover"],
+    answer: "C. onclick",
+  },
+];
--- JS/script.js
+++ JS/script.js
@@ -4,58 +4,103 @@ var timerId;
 
 var questionsElement = document.getElementById("questions");
 var timerElement = document.getElementById("time");
-var choiceElement = document.getElementById("choice");
+var choicesDivElement = document.getElementById("choice");
 var startBtn = document.getElementById("start");
+var scoreElement = document.getElementById("score");
+var resultsDiv = document.querySelector(".results");
+var initialsInput = document.querySelector(".initials");
+var submitBtn = document.querySelector(".submit-btn");
+var playBtn = document.querySelector(".play");
+var highScoreList = document.querySelector(".playerScores");
+var highScoresContainer = document.querySelector(".score-list");
+var localStorageScores = JSON.parse(localStorage.getItem("scores")) || [];
+
+console.log(localStorageScores);
+
+submitBtn.addEventListener("click", function () {
+  submitScoreToLocalStorage();
+  viewHighScoresFromStorage();
+  // whatever
+});
+
+playBtn.addEventListener("click", start);
+startBtn.addEventListener("click", start);
 
 function start() {
-    var startScreenElement = document.getElementById("start-screen")
-    startScreenElement.setAttribute("class", "hide")
-    
-    questionsElement.removeAttribute("class")
+  highScoresContainer.setAttribute("class", "hide");
+  currentQuestionId = 0;
+  time = questions.length * 15;
+  var startScreenElement = document.getElementById("start-screen");
+  startScreenElement.setAttribute("class", "hide");
 
-    timerId = setInterval(clockTime, 1000)
-    
-    timerElement.textContent = time
-    displayQuestions();
-};
+  questionsElement.removeAttribute("class");
+
+  timerId = setInterval(clockTime, 1000);
+
+  timerElement.textContent = time;
+  displayQuestions();
+}
 
 function displayQuestions() {
-    var questionCurrent = questions[currentQuestionId];
-    var titleElement = document.getElementById("questions-title");
+  var questionCurrent = questions[currentQuestionId];
+  var titleElement = document.getElementById("questions-title");
 
-    titleElement.textContent = questions[currentQuestionId].title;
-    choiceElement.innerHTML = "";
-    console.log(titleElement)
-    questionCurrent.choice.forEach(function(choices, i){
-        var choicesElement = document.createElement("button");
-        choicesElement.setAttribute("class", "choice");
-        choicesElement.setAttribute("value", choices );
-        choicesElement.textContent = i + 1 + ". " + choices;
-        choicesElement.onclick = evaluateQuestion;
-        choiceElement.appendChild(choicesElement);
-    });
-};
+  titleElement.textContent = questions[currentQuestionId].title;
+  choicesDivElement.innerHTML = "";
+  console.log(titleElement);
+  questionCurrent.choices.forEach(function (choice, i) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute("class", "choice");
+    choiceBtn.setAttribute("value", choice);
+    choiceBtn.textContent = i + 1 + ". " + choice;
+    // answerButton.onclick = evaluateQuestion;
+    choiceBtn.addEventListener("click", evaluateQuestion);
+    choicesDivElement.appendChild(choiceBtn);
+  });
+}
 
-function evaluateQuestion(){
-    // if (this.value != questions[currentQuestionId].answer) {
-    //     time -= 15;
+function evaluateQuestion() {
+  if (this.value !== questions[currentQuestionId].answer && time >= 15) {
+    time -= 15;
+  }
 
-    //     if (time < 0) {
-    //         time = 0
-    //     }; 
-    //     timerElement.textContent = time;
-    // };
-    currentQuestionId++
-    if (currentQuestionId = questions.length) {
-        displayQuestions()
-    }
-};
+  if (currentQuestionId === questions.length - 1) {
+    clearInterval(timerId);
+    displayResults();
+  } else {
+    currentQuestionId++;
+    displayQuestions();
+  }
+}
+
+function displayResults() {
+  questionsElement.setAttribute("class", "hide");
+  resultsDiv.removeAttribute("class", "hide");
+  scoreElement.textContent = time;
+}
 
 function clockTime() {
-    time--
-    timerElement.textContent = time
-};
+  if (time === 1) {
+    displayResults();
+    clearInterval(timerId);
+  }
+  time--;
+  timerElement.textContent = time;
+}
 
-startBtn.onclick = start;
+function submitScoreToLocalStorage() {
+  var initials = initialsInput.value;
+  localStorageScores.push({ initials, score: time.toString() });
+  localStorage.setItem("scores", JSON.stringify(localStorageScores));
+  resultsDiv.setAttribute("class", "hide");
+}
 
-
+function viewHighScoresFromStorage() {
+  highScoresContainer.removeAttribute("class", "hide");
+  localStorageScores.forEach((score) => {
+    var scoreListItem = document.createElement("li");
+    scoreListItem.textContent = `Name: ${score.initials}, Score: ${score.score}`;
+    highScoreList.appendChild(scoreListItem);
+    // {name: RD, score: 23}
+  });
+}
